e720571c89279eaa6e08a4871eb1df612b33513a
msbecky5354 <msbgpt5354@gmail.com>
2026-05-24 23:41:47 +0800

    Update searchEngine.js

--- searchEngine.js
+++ searchEngine.js
@@ -20,23 +20,26 @@ function getExactMatch(q) {
 // 🧠 1. 將 Categories 包裝成 Function (完美解決 currentLang 載入順序報錯嘅 Bug！)
 // 呢個版本已經徹底清走晒所有重複字，以及會撞超市貨品嘅危險字根 (bb, app, ai, bot, 平, 貴, 凍)
 function getCategories() {
+    // 檢查 botResponses 是否存在，並根據 currentLang 攞數據
+    const responses = (typeof botResponses !== 'undefined' && botResponses[currentLang]) ? botResponses[currentLang] : {};
+    
     return [
-        { words: ['你好', 'hello', 'hey', '早晨', '嗨', '您好', '喂', '哈囉', '午安', '哈佬', 'good morning', 'good afternoon'], reply: uiText[currentLang].replyGreeting },
-        { words: ['多謝', 'thank', '謝謝', '唔該', 'thx', '谢谢', 'tq', 'ty', '感激', '感恩'], reply: uiText[currentLang].replyThanks },
-        { words: ['拜拜', '88', 'bye', '再見', '走先', 'cya', '掰掰', 'see ya', '下次見', '閃先', '早唞', 'good night', '晚安', 'goodnight'], reply: uiText[currentLang].replyBye },
-        { words: ['天氣', 'weather', '落雨', '好熱', '好凍', '凍死', '打風', '紅雨', '黑雨', '黃雨', '行雷', '打雷', '暴雨', '颱風', '熱死', '凍冰冰', '天晴', '落狗屎'], reply: uiText[currentLang].replySmallTalkExtra },
-        { words: ['好叻', 'smart', '好用', '好棒', 'good', '厲害', '犀利', '叻仔', '叻女', '好勁', '好掂', '世一', '好讚', 'awesome', 'brilliant', 'excellent', '好正', '超正'], reply: uiText[currentLang].replyPraise },
-        { words: ['笑話', 'joke', 'gag', '好悶', '悶悶地', '講笑', '無聊', '冇聊', '冇嘢做', '解悶', '搞笑', 'funny', 'boring', '悶死', '有冇搞錯', '𠱁下我', '幽默'], reply: uiText[currentLang].replyJoke },
-        { words: ['幾歲', '幾多歲', '做緊咩', '食飯', '識幾多', '得閒', '有空', '做緊乜', '食咗飯未', 'busy', 'free', '吹水', '傾偈'], reply: uiText[currentLang].replySmallTalkExtra },
-        { words: ['omg', 'oh my god', '我的天', '天呀', '救命', '痴線', '黐線', '哇塞', 'wow', '大鑊', '死啦', 'crazy', 'insane'], reply: uiText[currentLang].replyOmg },
-        { words: ['點用', '點樣', '點做', '幫手', '唔明', 'help', '如何', '唔識', '教學', '求救', 'sos', '教我', '點算', '指南', '操作', '點搞'], reply: uiText[currentLang].replyHelp },
-        { words: ['你是誰', '你係邊個', 'who are you', 'who r u', '咩名', '乜名', '乜水', '咩水', '邊位', '機器人', 'robot', 'chatbot', '人工智能', 'ai助手', '你係咩', '咩機', '咩黎', '咩嚟', '咩來'], reply: uiText[currentLang].replyWho },
-        { words: ['好啱', '啱呀', '對呀', '係呀', '沒錯', '同意', '正解', '講得好', 'yeah', 'yes', 'yup', 'yep', 'of course', '絕對', '當然', '係囉', '冇錯', '係咪'], reply: uiText[currentLang].replyAgreement },
-        { words: ['搵唔到', '找不到', '冷門', '冇貨', '缺貨', '冇啦', '斷貨', 'out of stock', '搵極都冇', '無貨', '售罄', '賣晒'], reply: uiText[currentLang].r_no_product },
-        { words: ['個資', '隱私', '私隱', '資料安全', '個人資料', '保密', '資料外洩', '俾人偷', '私穩'], reply: uiText[currentLang].r_privacy },
-        { words: ['提示', '鐘仔', 'alert', '追蹤', '到價', '通知', 'notify', '鬧鐘', '提我', '降價', '減價', '平咗', 'mark低'], reply: uiText[currentLang].r_alert },
-        { words: ['差價', '激抵', '唔抵', '好貴', '太貴', '平價', '便宜', '呃錢', '水魚', '好平', '超平', '抵買', '抵到爛', '最平', '最抵', '搵笨', '海鮮價', '暴利'], reply: uiText[currentLang].r_gap },
-        { words: ['畫面', '主螢幕', '安裝', '桌面', '加落去', '手機app', '下載', 'download', '主頁', '捷徑', 'shortcut', 'pwa', '裝app', '裝機'], reply: uiText[currentLang].r_home }
+        { words: ['你好', 'hello', 'hey', '早晨', '嗨', '您好', '喂', '哈囉', '午安', '哈佬', 'good morning', 'good afternoon'], reply: responses.replyGreeting },
+        { words: ['多謝', 'thank', '謝謝', '唔該', 'thx', '谢谢', 'tq', 'ty', '感激', '感恩'], reply: responses.replyThanks },
+        { words: ['拜拜', '88', 'bye', '再見', '走先', 'cya', '掰掰', 'see ya', '下次見', '閃先', '早唞', 'good night', '晚安', 'goodnight'], reply: responses.replyBye },
+        { words: ['天氣', 'weather', '落雨', '好熱', '好凍', '凍死', '打風', '紅雨', '黑雨', '黃雨', '行雷', '打雷', '暴雨', '颱風', '熱死', '凍冰冰', '天晴', '落狗屎'], reply: responses.replySmallTalkExtra },
+        { words: ['好叻', 'smart', '好用', '好棒', 'good', '厲害', '犀利', '叻仔', '叻女', '好勁', '好掂', '世一', '好讚', 'awesome', 'brilliant', 'excellent', '好正', '超正'], reply: responses.replyPraise },
+        { words: ['笑話', 'joke', 'gag', '好悶', '悶悶地', '講笑', '無聊', '冇聊', '冇嘢做', '解悶', '搞笑', 'funny', 'boring', '悶死', '有冇搞錯', '𠱁下我', '幽默'], reply: responses.replyJoke },
+        { words: ['幾歲', '幾多歲', '做緊咩', '食飯', '識幾多', '得閒', '有空', '做緊乜', '食咗飯未', 'busy', 'free', '吹水', '傾偈'], reply: responses.replySmallTalkExtra },
+        { words: ['omg', 'oh my god', '我的天', '天呀', '救命', '痴線', '黐線', '哇塞', 'wow', '大鑊', '死啦', 'crazy', 'insane'], reply: responses.replyOmg },
+        { words: ['點用', '點樣', '點做', '幫手', '唔明', 'help', '如何', '唔識', '教學', '求救', 'sos', '教我', '點算', '指南', '操作', '點搞'], reply: responses.replyHelp },
+        { words: ['你是誰', '你係邊個', 'who are you', 'who r u', '咩名', '乜名', '乜水', '咩水', '邊位', '機器人', 'robot', 'chatbot', '人工智能', 'ai助手', '你係咩', '咩機', '咩黎', '咩嚟', '咩來'], reply: responses.replyWho },
+        { words: ['好啱', '啱呀', '對呀', '係呀', '沒錯', '同意', '正解', '講得好', 'yeah', 'yes', 'yup', 'yep', 'of course', '絕對', '當然', '係囉', '冇錯', '係咪'], reply: responses.replyAgreement },
+        { words: ['搵唔到', '找不到', '冷門', '冇貨', '缺貨', '冇啦', '斷貨', 'out of stock', '搵極都冇', '無貨', '售罄', '賣晒'], reply: responses.r_no_product },
+        { words: ['個資', '隱私', '私隱', '資料安全', '個人資料', '保密', '資料外洩', '俾人偷', '私穩'], reply: responses.r_privacy },
+        { words: ['提示', '鐘仔', 'alert', '追蹤', '到價', '通知', 'notify', '鬧鐘', '提我', '降價', '減價', '平咗', 'mark低'], reply: responses.r_alert },
+        { words: ['差價', '激抵', '唔抵', '好貴', '太貴', '平價', '便宜', '呃錢', '水魚', '好平', '超平', '抵買', '抵到爛', '最平', '最抵', '搵笨', '海鮮價', '暴利'], reply: responses.r_gap },
+        { words: ['畫面', '主螢幕', '安裝', '桌面', '加落去', '手機app', '下載', 'download', '主頁', '捷徑', 'shortcut', 'pwa', '裝app', '裝機'], reply: responses.r_home }
     ];
 }
 
